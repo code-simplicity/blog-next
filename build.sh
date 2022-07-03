28234e025efe904481f1ef1e516fc27c421770e1
Romantic <dupyi0912@gmail.com>
2022-07-03 09:16:06 +0800

    feat: 配置dockerfile

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ echo "--------开始构建博客门户------->"
 pwd
 whoami
 #先停止原来老的，然后删除
-sudo docker stop blog-next
-sudo docker rm blog-next
+docker stop blog-next
+docker rm blog-next
 #删除原来老的image
-sudo docker rmi -f blog-next
+docker rmi -f blog-next
 #构建image
-sudo docker build -t blog-next .
+docker build -t blog-next .
 echo "启动新镜像"
 #启动
-sudo docker-compose up -d
+docker-compose up -d
